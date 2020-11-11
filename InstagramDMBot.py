3d8792a54ea45226b548370f7d76c7caef46265d
Osakpolor Obaseki <obasekiosa@gmail.com>
2020-11-11 10:15:55 +0100

    Update

--- InstagramDMBot.py
+++ InstagramDMBot.py
@@ -15,8 +15,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
 
-myemail = ""                # Enter in your email for Instagram inside the quotation marks
-mypassword = ""             # Enter in your password for Instagram inside the quotation marks
+# Enter in your email for Instagram inside the quotation marks
+myemail = ""
+
+
+# Enter in your password for Instagram inside the quotation marks
+mypassword = ""
+
+
+# friendusernames = ["fragrancenco_"]
 friendusernames = ['ms_shiona', 'daponiel_', 'bengeezy05', 'sir_kamcy', 'esteequine', 'jigiman2', 'abbad_ss', 'iamoyediran', 'franklynebere', 'javixy_', 'anee_jkd', 'lynnola_deliveryservices', 'adakolewilliam', 'edidiongb', 'gladchinke', 'okashausmanisah', 'awhitehope', 'sam_igho', 'eyebling_lashes', 'vickayenterprise', 'gbengda', 'hollardusycollection', 'ml_4fashion', 'rocity04', 'indianaprints', 'kel01vin', 'meerahcollections', 'menclassicstories', 'aloot2', 'heelenbones', 'jude_onochie', 'debrah_ni', 'solomon_okla_', '_sleek_ivy', 'sule_gram', 'winifredittah', 'oloye.official', 'bodapal', 'king_dolly_', 'olanrewaju_1010', 'uctru', 'gifted_biz', 'al_mansurr', 'enigmajackson5', 'sir_chuki007', 'iamaimar_', 'oludare.agboola.7', 'abdallahalamin4', 'ummyabullahi22', 'abjocreations', 'ferrarimalik', 'blckjackagency', 'liteskinned_renee', 'titilola_ogunniyan', 'lethalthamannx_2020', 'marysalami04', 'victor_endy', 'lumexbaba1991', 'mhiz_babyb', '25sweetlipz', 'funmi_akintoye', 'omoh_akin', 'merticdesigns_concept', 'jblaql', 'poseidon_wil', 'hair_by_lavicci', 'keem_gege', 'sotonye_69', 's_adam25', 'saifdamrah', 'stanbaba1', 'joke_bolaji', 'toyosi_adebiyi78', 'iam_tina32', 'jamil_jameeel', 'printsdon', 'lloyd2great', 'judith_etimetuk', 'aali__omar', 'asphodel_tulip', '_nenyeee', 'adamuakanimo', 'theoboapea', 'tsbellebeddings_et_al', 'smoothok', '_kanyinsola__', 'simplyclassykiddies', 'mcjblazedwfc', 'hdperfumery', '_________jordan___', 'dorothyshairhub', '_durroothee_', 'mosunmola_a', 'oyeyemi_dominion_adetu2adetule', 'lionandjewelonline', 'sugar__boii', 'lamba_01', 'bakedbyrem', 'kecho78', 'estie202', 'ttoniabiks', 'yahuza_hair_empire', 'oluwatobilobaderinola', 't_yank2000', 'dedec1547', 'forever.pearl5', 'iam_goks', 'beautynfragrancestore', 'sopblisz', 'ummihsan4', 'sumiee__', 'tunde_junaid', 'oliveahanotu', 'benuequeen', 'naccim', 'modupeonitilo', 'baron_fxpro', 'chudy_ctoc', 'gracious_glo', 'minoddyy', 'paulchristopher9', 'monsieurcharle', 'bobbyy.o', 'lakebakare', 'dike_eji_eje_mba', 'outstandinganita', 'azeezah.ibrahim', 'bunmi_olalekanodusanya_', 'zee_perfume_oil', 'justvican', 'mizt.omi', 'mz_mooreoluwa', 'figo9234', 'wiigsbyrez', 'damolafapohunda', 'omoruyiodigie', 'tosynes_accessories', 'bukky1016', 'fahadlawal', 'tomisalau', 'pancakeplug_ng', 'zouks_love', 'joanne_agbam', 'luxurycarsnaija', 'mabraz_trading_company', 'okeybenebs', 'eversmilingglory', 'mr_yousouph', 'jemqween', 'i_am_mermie', 'n.k_lovingherself', 'linda_durkwa', 'jayneassydavid', 'footetish_angel', 'chid_18', 'queen_libby_diva', 'fwanaomi', 'perfume_oils_and_more', 'skinglownaija', 'mistrpeters', 'optimuzprim', 'abdulmumeenandidi', 'momma_zicon', 'diamondwholesalebottles2', '_ladybecks', 'king.shallangwa', 'francc1822', 'fiscophinna01', 'billysgourmetbites', 'ade_daayo', 'dercio.tivane', 'dinoh03', 'ganijay6backupaccount', 'nwaaneke', 'mz_orla', 'ingenious_lawpractitioner', 'house_of_sleeks', '_sandiescent', 'wura_golddy', 'luchyjohn2019', 'ganijay6', 'samuelgodfrey_2020', 'lilylistick12', 'wuragoldy', 'chic_hibu', 'konadu956', 'kvng.rosel', 'babygirlmolly7', 'harxeehpeeta', 'xingxing2021', 'huncho_okes', 'iam_royaltyy', 'adewuyi007', 'ayobamiokla', 'itz_yetty', 'adedokun_jnr1', 'dee_don111', 'priceless_rejoice', 'blesco95.backup', 'chizzbela', 'welmiss2000', 'totalbrideperfumery', 'fikky_fiks', 'maestrodarlingtonxornam10', 'tembeth1', 'kehinde8555', 'annie__hairline', 'lumi_decent', 'melodilisa', 'scentbyoreo', 'mays.fragrances', '64_logistics', 'ziy.closet', 'miraonyebuchi', 'olami_sleek', 'popo_fabrics', 'major.mercy', 'unique_love222', 'sendcityng', 'designer_perfume_mall', 'wholesale_bottles', 'boss__davis1', 'shopbikini.wears', 'pweedieweani', 'alade_honey', 'vaniciaga_', 'rainbow_queeness', 'ope_debss', 'e_faith_196', 'delivery_briskwheel', 'luxeandpearl', 'thitosfragrance', 'triple_henri', 'wardrobeandclosets', 'a_ha256', 'teejuubee', 'ty_daily_store', 'fayisco_jnr', 'mischi_vieous', 'michaelj641', 'ameesweets', 'thelaja', '4kto9k_thriftshoes', 'johnson.omowumi.338', 'detutu__', 'iamwhale_x', 'boss_davis1', 'zarit_hoof', 'damiphilip', 'tytylayomi_07', 'it_isperry', 'slymanraheel', 'tribes_luxury_scents', 'kareh_liyah', 'cazonfoods', 'majesticstylez.biz', 'ajoke_comedy', 'testiheart', 'doyinsbrand', 'almaas_stylishflow', 'fay_scents', 'indianahair_lounge', 'amyluxuryhair', 'damilolafolaazeez', '_wunmii', 'bash1y2k', '19thavenue_ng', 'ajibooh', 'thelittleslipper', 'lusciousseries', 'vida_mariam', 'boujeevendorlists', 'afolashade.afolashade.96', 'thefeet.affairs', 'luvlidoo', 'fragrancebanks', 'theofficialscents', '_rantimi', 'bavic.cosmetic', '__musteey_sherheed', 'mz_ihekaru', 'rosesbedding', 'sophisticatedwristwatch',
                    'sowphs_fragrance', 'perfsbyash', 'theslickclothing', 'renihairline', 'queeneth_vicky', 'shilarr', '5r_concept', 'praiyz_neye', 'relisher_scents', 'yemwatches', 'oilsbyash_', 'shoprave_ng', 'irepitan_', 'malo_soldier', 'magnetique_eventss', 'ofada_koj1', 'hestercowan62', 'hardrockinteriordecorations', 'official_unisce_oni', 'firstgradethriftukaywears', 'korede__o', 'mbbs_bds2020', 'loglash_catering_and_events', 'zobas_hairs', 'paloma_peeter', 'mide.accessories', 'zelumosonwa_', '1kofficebutik', 'iiignacystore', '_opeyemibalogun', 'glowbyash.ng', 'ravishingsurprise_ng', 'annie_chuks_', 'foodabattoir', 'house_of_regeenah', 'p_e_j_u_', 'usman_phina', 'chigoziechinye1', 'blessedotas57', 'aigbovocelina', 'mercyugonick', 'sandilicious4488', 'elizabethfred09', 'lawretta8', 'chibuzonkem27', 'maryebose8', 'princessvickky09', 'feyioluwa75', 'jennyeve09_', 'loveangel9806_', 'pino.pino123', 'cha.chi123', 'anugemaris', 'ifeanyifranca', 'abuunyandu', 'setphenwisdom', 'frank18359', 'joyruth38', 'amaraabigial', 'onyinyechi3746', 'wakeandwalkout', 'nathanprecious8540', 'precikul', 'ezealochibuike', 'veracious_2000', 'nkechi_ruth', 'bathandbodyng', 'foby_oil', 'ife__scents', 'towosworld', 'writer_reo', 'leonsecrets_', 'moyinoluwa_z', 'blissfulessence_', 'yegbraidsby_gabii', 'mr_aktofficial', 'inspiredoutfitss', 'fruitie_delites', 'jacck_logo', 'stephanie__asaah', 'chiefnaa43', 'tz_itunu', 'kessifancy', 'jessyberry28', 'perfectfeetbybb', 'christy_love20', 'aceyemay', 'chinenye____', 'amhani_', 'valerieobaseki', 'mamijay2_', 'patiencejonathan418', 'blessingobim888', 'omoyeemmanuel69', 'charlotteobose', 'chinekezieb', 'lovelytimi987_', 'janeteddy3800', 'celinay551', 'kimanderson0191', 'ifesinachinnenna', 'victoriaebiye_', 'victoriaebiye__', 'preciousigho4', '_fhavourrr', 'sweetestbrenda123',
                    'faithbugatti', 'sorpheeyah_', 'queen_abiodun_ogunnika', 'every_thinghousehold', 'irefoods', 'the14oranges', 'loveangel9806', 'africanweddingvibes', 'worldwideslaykids', 'topup9ja_instagain', 'stebenz_stitches',
@@ -29,16 +36,25 @@ friendusernames = ['ms_shiona', 'daponiel_', 'bengeezy05', 'sir_kamcy', 'esteequ
                    'mizpah_fm', 'onenineja', 'the_print_guru', 'beebah_fabrics', 'pearlsapparelng', 'bimladwears83', 'everything_bimmy', 'uc_morezphones', 'houzofjazz', 'pennek_nigeria', 'ay_agbaje', 'adrielladimples', 'coldcubeice', 'july5th_empire', 'zadoya_makeovers', 'coreopscleaners', 'gracies_fashion_empire', 'cottsframeng', 'odunayoelisa', 'chinawholesales_', 'martcribz', 'cheetah2730', 'fragrancetips', '_taayo', '_abebii_', 'tonbratoms', 'abiolaayomikun', 'lase_moye', 'the_iseoluwa', '43forex_academy', 'thedreammonetizer', 'abbycutiee', 'lady.p_clothing', 'bestpricekiddiesstore', 'tboygabzy', 'pmndifon', 'rukkybellaz', 'mz_cutiebear', 'ezehebuka48gmi', 'tinuade_ayomide', 'amologistics', 'dayveed_xx', 'classiekiddies', 'irene_lanez', 'its_niphe', 'rolyguy', 'jhnsinos', 'officialclems', 'meenahs_palace', 'gadgetsandco_', 'itunuy', 'agathalegal', 'caramella____', '_gaming_bruder_', 'brideandmaidsfactory', 'six_25butik', 'gcseducation', 'olusolaamusan', 'iconic_faces_ng', 'jamie_watch', 'ldm_cosmetics', 'ibrahimisa2828', 'mj_kids_apparel', 'joyslizzy', 'emillie_jayne_bespoke', 'demisyhair', 'marygolding85', 'dorothy_jaja', 'ayoarigbs', 'lyr_accessories', 'henriblog', 'thehustlespiceking', 'houseofdivas.ng', 'nellz_exclusive', 'southsouth_vibes', 'chevonne_cosmetics', 'ifuskin.solutions', 'anthurium_hotel', 'sakaila_hairs', 'morena_jay', 'affordable_fab', 'ivannas.collections', 'sales_curve', 'elenacova_', 'snoopiewears', 'the_niceshopping', 'kokz_collection', 'souvenirsbygold', 'dannabeauty_', 'dollyqueeneebeautypro', 'unikredoil', 'faith_ezennaya', 'ogawilliamswaltd', 'jesseytouch', 'boluwxtife', 'klasick_beads', 'hayjay_organics', 'cupideventsng', 'tarila_ajuesi', 'zeeky_cakes', 'blessingezaka', 'ozegberic', 'dr_snowyt', 'ewardunni', 'kelsey_makeovers', 'aomintegrated', 'strutinlanez', 'dbookplace', 'becky_bbeauty', 'lagosexpo', 'aquaklens', 'tboclothings', 'celemat', 'obeyaluminium8', 'vicci_bakare', 'brightlandproperties', 'asmonpeters', 'fematechgadget_', 'king_kekere', 'glamourstores_', 'sekina_adeyemi', 'hasmet_dayi', 'yetundedada74', 'iceyfashionstore', 'officialbusolami', 'youngboss_fanpage_', 'educated_thuggg', 'chinawholesale_kiddiesitems', 'iamtosintaylor', 'dbautohire', 'gabbyjay20', 'orelovesoreo', 'jou.glam', 'ebookmentor', 'regeenah_dee', 'china_importexpert', 'hizzangelradow', '_nenye__',
                    'chynonxo', 'gimplexlogistics', 'im_chichii', 'beepmedia47', 'iamelegant01', 'gbeminola', 'cozydecor.ng',
                    'totallytrendy_by_lammy', 'damigoldclothings', 'oomochioo666', 'oliviadavidsluxuries', 'sweetgenre_cakes', 'gamebreakerr', 'royalbeddingnaija', 'r_e_a_l_s_a_m', 'royalvenice', 'mappipgadgets', 'favygiant', 'sugarcyril', 'ceejayikpeze', 'olawayee', 'ransomeautos', 'yhumiesaxophonist', 'minuelfaleti', 'femiadedeji', 'clameliaservices', 'f__ua__d', 'roysavivastore', 'officialnictv', 'choiceassistantltd', 'amaafashion', 'princessroseyb', '_amaraaaa_', 'omobolafad', 'alayoni_adee', 'prettyberri', 'sales_connect', 'tatahairsolutions', 'futuresoftng', '_modemartins', 'wolejunior', 'bidsglam', 'titan_apparels', 'owanbe_vendors', 'otse__', 'naoslay', 'feragostyle', 'duro_teemy', 'mrbanks_official', 'allure_express', 'luisz5th', 'akes_hub', 'teemsglobal', 'busayos_place', 'walitees', 'decluttercabin', 'vivian_nwosu', 'naturelover__007', 'bassetclothings', 'greenlottong', 'kleo_products', 'sunshinepantryhouse', 'samwelgachuhikaimba', 'temitope_fadare', '_hennabyhafsah', 'topsecret_hairplanet', 'veedahlia', 'hephzibahs_collections', 'morgans_stores', 'tiri.ng', 'thewears_mall', 'chi__ora', 'macedigital', 'niklar_world', 'onpointwristwatches', 'jaydizworld', 'novakids_', 'undress_stores', 'tanelaerrands', 'atad_luxury', 'deevapreneur', 'styleswitch.ng', 'allroundcargocompany', 'deedees_collection_']      # Enter in the username of the recipient inside the quotation marks! To send this to mutiple users, seperate users by commas! Ex)  friendusernames = ["@friend1", "@friend2", "@friend3"]
+
 numoftimes = "1"             # Enter in the number of times you would like to send the message to the recipient Ex) "2"
 
 # The message being sent
-message = "Automated tests Again"
+messages = ["Hello and good day dear follower / potential customer. This is FragranceNco _\n", "Our previous account(@fragragrancenco)was hacked, and we may not be able to retrieve it.\n", "Kindly follow this new one(@fragrancenco_).\n", "We would love you to have you back, and we would also love you to help us build our page back from the scratch.\n",
+            "That’s why every little follow matters.\n", "Thank you for staying with us so far, let’s do it again.\n", "We would be posting more so you can see new stock and enjoy your shopping journey.\n", "Please Disregard any fraudulent message from our previous page(@fragrancenco).\n", "The username on that old page now is (@nicole_andrea_fx__)\n", "Please do not under any circumstance, pay money to anyone with that name or from that account to avoid getting scammed.\n", "Love, FragranceNco.\n"]
+
 
 # Step 4 of the installations instructions
-PATH = "" #path to where the driver is 
+# PATH = "" #path to where the driver is 
+
+
+PATH = "D:\drivers\chromedriver_win32\chromedriver.exe" ## an example
 
 driver = webdriver.Chrome(PATH)
 # driver = webdriver.Chrome(executable_path=r'C:/path/to/chromedriver.exe') //an example
+
+offset = 110
+MAX_RANGE = 50 # instagram seems to limit to about 110 messages so you can change it from 50 to 110
 
 url = "https://www.instagram.com/"
 driver.get(url)
@@ -69,53 +85,61 @@ try:
     except:
         print ("Could not click not now on the notifications pop up!")
 
-    for friendusername in friendusernames:
-        try:
-            searchuser = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.EQ1Mr')))
-            searchuser.click()
-        except:
-            print ("Could not click on the new message button!")
-        try:
-            searchuserbox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.j_2Hd')))
-            searchuserbox.send_keys(friendusername)
-        except:
-            print ("Could not find the enter username box!")
+    length = len(friendusernames)
+    for index in range(MAX_RANGE):
+        count = offset + index
+        if count < length:
+            friendusername = friendusernames[count]
+            try:
+                searchuser = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.EQ1Mr')))
+                searchuser.click()
+            except:
+                print ("Could not click on the new message button!")
+            try:
+                searchuserbox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.j_2Hd')))
+                searchuserbox.send_keys(friendusername)
+            except:
+                print ("Could not find the enter username box!")
 
-        try:
-            # time.sleep(5)
-            time.sleep(2)
-            # firstuser = driver.find_element_by_css_selector('.HVWg4')
-            firstuser = driver.find_element_by_css_selector('.dCJp8 ')
-            print(firstuser)
-            firstuser.click()
-        except:
-            print("Could not click on the first user!")
+            try:
+                # time.sleep(5)
+                time.sleep(2)
+                # firstuser = driver.find_element_by_css_selector('.HVWg4')
+                firstuser = driver.find_element_by_css_selector('.dCJp8 ')
+                # print(firstuser)
+                firstuser.click()
+            except:
+                print("Could not click on the first user!")
 
-        try:
-            pressingnext = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.rIacr')))
-            pressingnext.click()
-        except:
-            print ("Could not press \"Next\"!")
+            try:
+                pressingnext = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.rIacr')))
+                pressingnext.click()
+            except:
+                print ("Could not press \"Next\"!")
 
-        try:
-            messagebox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.focus-visible')))
-            messagebox.click()
-        except:
-            print ("Could not find the text box!")
+            try:
+                messagebox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.focus-visible')))
+                messagebox.click()
+            except:
+                print ("Could not find the text box!")
 
-        try:
-            # Finds the text box
-            textbox = driver.find_element_by_css_selector('.focus-visible')
-        except:
-            print("Could not find the text box!")
+            try:
+                # Finds the text box
+                textbox = driver.find_element_by_css_selector('.focus-visible')
+            except:
+                print("Could not find the text box!")
 
-        try:
-            # Sends the message
-            for i in range(int(numoftimes)):
-                textbox.send_keys(message)
-                textbox.send_keys(Keys.RETURN)
-        except:
-            print("Error sending the message!")
+            try:
+                # Sends the message
+                for i in range(int(numoftimes)):
+                    for message in messages:
+                        textbox.send_keys(message)
+                        textbox.send_keys(Keys.RETURN)
+                    # textbox.send_keys(message1)
+                    # textbox.send_keys(Keys.RETURN)
+                print(f"Completed {count + 1} users")
+            except:
+                print("Error sending the message!")
 
 except:
     print("An error has occurred")
